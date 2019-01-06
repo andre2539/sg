406975550a9efe54d6420f8449051a1e023150e3
andre2539 <avaricleusdf@gmail.com>
2019-01-06 11:59:32 +0700

    Adding some new trackers

--- config_blank.php
+++ config_blank.php
@@ -21,6 +21,14 @@ $CONFIG = array(
         'udp://tracker.opentrackr.org:1337/announce',
         'udp://tracker.zer0day.to:1337/announce',
         'udp://tracker.leechers-paradise.org:6969/announce',
-        'udp://coppersurfer.tk:6969/announce'
+        'udp://coppersurfer.tk:6969/announce',
+        'udp://tracker.internetwarriors.net:1337/announce',
+        'udp://tracker.leechers-paradise.org:6969/announce',
+        'udp://tracker.coppersurfer.tk:6969/announce',
+        'udp://tracker.pirateparty.gr:6969/announce',
+        'udp://tracker.port443.xyz:6969/announce',
+        'http://explodie.org:6969/announce',
+        'http://torrent.nwps.ws/announce',
+        'udp://tracker.cyberia.is:6969/announce'
     )
 );
